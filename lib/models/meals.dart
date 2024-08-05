c98d781f256d0ac202e436fc3e9deb92e1c69021
adogbeji <baka_1993@yahoo.co.uk>
2024-08-05 13:57:37 +0100

    Add more variables & initialise them in constructor of Meal class

--- lib/models/meals.dart
+++ lib/models/meals.dart
@@ -12,7 +12,27 @@ enum Affordability {
 
 class Meal {
 
+  Meal({
+    required this.id,
+    required this.categories,
+    required this.title,
+    required this.duration,
+    required this.complexity,
+    required this.affordability,
+    required this.isGlutenFree,
+    required this.isLactoseFree,
+    required this.isVegan,
+    required this.isVegetarian,
+  });
+
   final String id;
   final List<String> categories;
   final String title;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegetarian;
 }
