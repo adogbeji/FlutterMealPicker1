ea0ea61bb2e7ae68c3981161c5402eba91dc03ca
adogbeji <baka_1993@yahoo.co.uk>
2024-08-06 12:12:25 +0100

    Create function to show snackbar in Meals widget

--- lib/views/widgets/meals.dart
+++ lib/views/widgets/meals.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:meal_picker_1/models/meals.dart';
+
 class Meals extends StatefulWidget {
   const Meals({super.key});
 
@@ -8,6 +10,18 @@ class Meals extends StatefulWidget {
 }
 
 class _MealsState extends State<Meals> {
+  final List<Meal> _favouriteMeals = [];  // Stores favourite meals
+
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();  // Removes any existing info messages ("snack bars")
+  
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
