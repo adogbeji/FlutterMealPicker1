c88204a2790c9614d2a56dc6689475f96aa53688
adogbeji <baka_1993@yahoo.co.uk>
2024-08-07 14:00:25 +0100

    Start adding dummy data & install flutter_riverpod package

---
+++ lib/models/data/dummy_data.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:meal_picker_1/models/category.dart';
+import 'package:meal_picker_1/models/meals.dart';
+
+final availableCategories = [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    colour: Colors.deepPurple
+  ),
+  Category(
+    id: 'c2',
+    title: 'Hamburgers',
+    colour: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'German',
+    colour: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Light & Lovely',
+    colour: Colors.blue,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Exotic',
+    colour: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Breakfast',
+    colour: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asian',
+    colour: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c9',
+    title: 'French',
+    colour: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Summer',
+    colour: Colors.teal,
+  ),
+];
+
+
+final dummyMeals = [
+  Meal(
+    id: 'm1', 
+    categories: ['c1', 'c2'], 
+    title: 'Spaghetti with Tomato Sauce', 
+    duration: 20, 
+    complexity: Complexity.easy, 
+    affordability: Affordability.affordable, 
+    isGlutenFree: false, 
+    isLactoseFree: true, 
+    isVegan: true, 
+    isVegetarian: true,
+  ),
+];
