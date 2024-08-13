2154c8e05a1f459c67b45c707095ea4a504c9790
adogbeji <baka_1993@yahoo.co.uk>
2024-08-13 12:22:05 +0100

    Add more code to filters provider & start using it in Meals widget

--- lib/controllers/providers/filters_provider.dart
+++ lib/controllers/providers/filters_provider.dart
@@ -37,4 +37,23 @@ final filtersProvider = StateNotifierProvider<FiltersNotifier, Map<Filter, bool>
 final filteredMealsProvider = Provider((ref) {
   final meals = ref.watch(mealsProvider);
   final activeFilters = ref.watch(filtersProvider);
+
+  return meals.where((meal) {
+    if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+      return false;
+    }
+
+    if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+      return false;
+    }
+
+    if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+
+    if (activeFilters[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+    return true;
+  }).toList();
 });
--- lib/views/widgets/meals.dart
+++ lib/views/widgets/meals.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meal_picker_1/controllers/providers/filters_provider.dart';
 
 import 'package:meal_picker_1/models/meals.dart';
 
-class Meals extends StatefulWidget {
+class Meals extends ConsumerStatefulWidget {
   const Meals({super.key});
 
   @override
-  State<Meals> createState() => _MealsState();
+  ConsumerState<Meals> createState() => _MealsState();
 }
 
-class _MealsState extends State<Meals> {
+class _MealsState extends ConsumerState<Meals> {
   // final List<Meal> _favouriteMeals = [];  // Stores favourite meals
 
   void _showSnackBar(String message) {
@@ -24,6 +26,8 @@ class _MealsState extends State<Meals> {
 
   @override
   Widget build(BuildContext context) {
+    final availableMeals = ref.watch(filteredMealsProvider);
+
     return Scaffold(
       body: Center(
         child: Text('Meals'),
